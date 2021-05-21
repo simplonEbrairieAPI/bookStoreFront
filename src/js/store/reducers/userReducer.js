c68c2ef54b2aea701e137f5bbbf68c44227b45f4
Valery <chinvalery@gmail.com>
2021-05-21 14:40:28 +0200

    [state]set initial state when logout

--- src/js/store/reducers/userReducer.js
+++ src/js/store/reducers/userReducer.js
@@ -1,8 +1,8 @@
 import { SIGN_IN, LOAD, LOGOUT } from '../actions/userActions';
 
 const initialState = {
-  user: null,
   isAuthenticated: false,
+  isLoading: false,
   userFirstName: null,
   userLastName: null,
   userEmail: null
@@ -10,28 +10,23 @@ const initialState = {
 
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
-    case SIGN_IN:
-      // console.log("userId", action.cookie.userId)
+    case LOAD:
       return {
         ...state,
+        isLoading: true,
+      };
+    case SIGN_IN:
+      return {
+        ...state,
+        isLoading: false,
         isAuthenticated: true,
         userFirstName: action.payload.firstName,
         userLastName: action.payload.lastName,
         userEmailName: action.payload.email,
-
-
-        //  userId: 
       };
-    case LOAD:
-      return {
-        ...state,
-        isAuthenticated: true,
-      }
+
     case LOGOUT:
-      return {
-        ...state,
-        isAuthenticated: false,
-      };
+      return initialState;
 
     default:
       return state;
